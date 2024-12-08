4e5561b36a185338ae8f8bbef76541f9d4d9b35e
Hiroshi Hagiwara <mukakinkatsubou@gmail.com>
2024-12-08 12:39:15 +0900

    fix:

--- webapp/sql/1-schema.sql
+++ webapp/sql/1-schema.sql
@@ -135,7 +135,7 @@ CREATE TABLE coupons
 )
   COMMENT 'クーポンテーブル';
 
-ALTER TABLE ride_statuses ADD INDEX ride_id_idx (ride_id, created_at DESC)
-ALTER TABLE chairs ADD INDEX owner_id_idx (owner_id)
-ALTER TABLE chair_locations ADD INDEX chair_id_order_idx (chair_id, created_at)
-ALTER TABLE rides ADD INDEX chair_id_order_idx (chair_id, updated_at DESC)
+ALTER TABLE ride_statuses ADD INDEX ride_id_idx (ride_id, created_at DESC);
+ALTER TABLE chairs ADD INDEX owner_id_idx (owner_id);
+ALTER TABLE chair_locations ADD INDEX chair_id_order_idx (chair_id, created_at);
+ALTER TABLE rides ADD INDEX chair_id_order_idx (chair_id, updated_at DESC);
