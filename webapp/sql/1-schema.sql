a2118c67cd94ab4b44533941a259deb5d2e36abe
Hiroshi Hagiwara <mukakinkatsubou@gmail.com>
2024-12-08 12:53:43 +0900

    fix:

--- webapp/sql/1-schema.sql
+++ webapp/sql/1-schema.sql
@@ -139,3 +139,6 @@ ALTER TABLE ride_statuses ADD INDEX ride_id_idx (ride_id, created_at DESC);
 ALTER TABLE chairs ADD INDEX owner_id_idx (owner_id);
 ALTER TABLE chair_locations ADD INDEX chair_id_order_idx (chair_id, created_at);
 ALTER TABLE rides ADD INDEX chair_id_order_idx (chair_id, updated_at DESC);
+ALTER TABLE chairs ADD INDEX access_token_idx (access_token);
+ALTER TABLE rides ADD INDEX user_id_order_idx (user_id, created_at DESC);
+ALTER TABLE coupons ADD INDEX used_by_idx (used_by);
